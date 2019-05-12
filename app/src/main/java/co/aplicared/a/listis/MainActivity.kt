5a105fa48106a4f51aefa604da6dfabaf6de249a
Louis Hollingworth <louis@aplicared.co>
2019-05-12 20:07:00 +0100

    Added MainActivity and Card Designs

---
+++ app/src/main/java/co/aplicared/a/listis/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.aplicared.a.listis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
